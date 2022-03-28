2773e92642b2ed21a93d8abe81741dbe0f01ac70
Wind Slightly <70873548+iamSlightlyWind@users.noreply.github.com>
2022-03-28 14:40:41 +0700

    try further optimize workflow because why not

--- megacmd.sh
+++ megacmd.sh
@@ -4,6 +4,6 @@ echo "https://anonfiles.com/Ldt433R9x0/megacmd-xUbuntu_20.04_amd64_deb" > link
 
 curl https://raw.githubusercontent.com/henrydatei/anonfile-direct-link/master/anonfile_direct_link.sh > get.sh
 
-aria2c -x2 $(sh get.sh link)
+wget $(sh get.sh link)
 
 rm link get.sh
