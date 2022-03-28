8747a7859351372ec68c7166efb5d610d96a3c27
Wind Slightly <70873548+iamSlightlyWind@users.noreply.github.com>
2022-03-28 14:43:41 +0700

    try furtheeeeer

--- megacmd.sh
+++
@@ -1,9 +0,0 @@
-#https://github.com/henrydatei/anonfile-direct-link/blob/master/anonfile_direct_link.sh
-
-echo "https://anonfiles.com/Ldt433R9x0/megacmd-xUbuntu_20.04_amd64_deb" > link
-
-curl https://raw.githubusercontent.com/henrydatei/anonfile-direct-link/master/anonfile_direct_link.sh > get.sh
-
-wget $(sh get.sh link)
-
-rm link get.sh
